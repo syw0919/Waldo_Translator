29351a890fb884ec0c79527bac46b1dce5b19c8d
Young-woong Lazarus Sohn <44917049+syw0919@users.noreply.github.com>
2021-03-25 11:40:58 +0900

    [ update ]

--- app.py
+++ app.py
@@ -141,15 +141,6 @@ async def on_message(message):
                 options.add_argument('--headless')
                 options.add_argument("--disable-gpu")
                 options.add_argument("--window-size=1280x1696")
-                options.add_argument("--disable-application-cache")
-                options.add_argument("--disable-infobars")
-                options.add_argument("--no-sandbox")
-                options.add_argument('--disable-dev-shm-usage')
-                options.add_argument("--hide-scrollbars")
-                options.add_argument("--enable-logging")
-                options.add_argument("--log-level=0")
-                options.add_argument("--single-process")
-                options.add_argument("--ignore-certificate-errors")
                 driver = webdriver.Chrome('/app/.chromedriver/bin/chromedriver', options=options)
 
                 wait = .5
