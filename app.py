815efdc038da79bd3f7fe6bd0b195854666bd5b0
Young-woong Lazarus Sohn <44917049+syw0919@users.noreply.github.com>
2021-03-25 14:06:21 +0900

    [ update ]

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from googletrans import Translator
-from selenium import webdriver
-import time
 import asyncio
 import discord
 import os
@@ -122,87 +120,6 @@ async def on_message(message):
                 await channel.send('그 입력 은 잘못된 형태의!')
         else:
             await channel.send('지금 말투: {}'.format('자연스러운' if mode == 1 else '역겨운'))
-
-        date = message.created_at
-        print('[ {} ]'.format(message.author))
-        print('[ {:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d} ]'.format(date.year, date.month, date.day, date.hour, date.minute, date.second))
-        print(message.content)
-
-    if message.content.startswith('!자가진단'):
-        ret = 1
-        while ret != 0:
-            print('자가진단 매크로가 시작되었습니다.')
-            await channel.send('자가진단 매크로가 시작되었습니다.')
-
-            try:
-                options = webdriver.chrome.options.Options()
-                options.add_argument('--headless')
-                options.add_argument("--disable-gpu")
-                options.add_argument("--start-maximized")
-                driver = webdriver.Chrome('/app/.chromedriver/bin/chromedriver', options=options)
-
-                wait = .5
-
-                driver.get('https://hcs.eduro.go.kr/') # 사이트 접속
-                await channel.send('사이트 접속')
-                time.sleep(1)
-                driver.find_element_by_id('btnConfirm2').click() # 자가진단 참여하기 버튼 클릭
-                time.sleep(wait)
-                driver.find_element_by_class_name('searchBtn').click() # 학교 검색
-                time.sleep(wait)
-                driver.find_element_by_xpath('//*[@id="softBoardListLayer"]/div[2]/div[1]/table/tbody/tr[1]/td/select/option[7]').click() # 대전광역시 선택
-                await channel.send('대전광역시 선택')
-                driver.find_element_by_xpath('//*[@id="softBoardListLayer"]/div[2]/div[1]/table/tbody/tr[2]/td/select/option[5]').click() # 고등학교 선택
-                await channel.send('고등학교 선택')
-                driver.find_element_by_class_name('searchArea').send_keys('대덕소프트웨어마이스터고등학교\n') # 대덕소프트웨어마이스터고등학교 검색
-                await channel.send('대덕소프트웨어마이스터고등학교 검색')
-                time.sleep(wait)
-                driver.find_element_by_xpath('//*[@id="softBoardListLayer"]/div[2]/div[1]/ul/li/a/p/a').click() # 검색 결과 선택
-                driver.find_element_by_xpath('//*[@id="softBoardListLayer"]/div[2]/div[2]/input').click() # 학교선택 버튼 클릭
-                time.sleep(wait)
-                driver.find_element_by_id('user_name_input').send_keys('손영웅') # 이름 입력
-                await channel.send('이름 입력')
-                driver.find_element_by_id('birthday_input').send_keys('030919\n') # 생년월일 입력
-                await channel.send('생년월일 입력')
-                time.sleep(wait*4)
-                driver.find_element_by_xpath('//*[@id="WriteInfoForm"]/table/tbody/tr/td/input').send_keys('1234\n') # 암호 입력
-                await channel.send('암호 입력')
-                time.sleep(wait*6)
-                driver.find_element_by_class_name('name').click() # 손영웅 클릭
-                time.sleep(wait*4)
-                try:
-                    alert = driver.switch_to.alert
-                    message = alert.text
-                    alert.accept()
-                    driver.close()
-                    print('Alert창: ' + message)
-                    await channel.send('Alert창: ' + message)
-                    time.sleep(3)
-
-                except Exception as ex:
-                    print('[ Info ]\n로그인 완료')
-                    await channel.send('[ Info ]\n로그인 완료')
-
-                driver.find_element_by_id('survey_q1a1').click() # 코로나 의심 증상 없음
-                await channel.send('코로나 의심 증상: 아니오')
-                driver.find_element_by_id('survey_q2a1').click() # 코로나 검사 결과를 기다리고 있지 않음
-                await channel.send('코로나 검사 결과 대기 여부: 아니오')
-                driver.find_element_by_id('survey_q3a1').click() # 자가격리가 이루어지고 있지 않음
-                await channel.send('자가격리 여부: 아니오')
-                driver.find_element_by_id('btnConfirm').click() # 제출 버튼 클릭
-                await channel.send('제출')
-                time.sleep(2) if options.headless else time.sleep(5)
-                print('[ Info ]\nTask failed successfully')
-                await channel.send('[ Info ]\nTask failed successfully')
-                ret = 0
-
-            except Exception as ex:
-                print(f"[ Error ]{str(ex).rstrip()}\n\n")
-                await channel.send(f"[ Error ]{str(ex).rstrip()}\n\n")
-                driver.close()
-
-            finally:
-                driver.close()
 
         date = message.created_at
         print('[ {} ]'.format(message.author))
