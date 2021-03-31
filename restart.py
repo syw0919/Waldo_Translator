53149b1ff97da39a03bf3fba83c6d32179f093b2
Young-woong Lazarus Sohn <44917049+syw0919@users.noreply.github.com>
2021-03-31 14:52:08 +0900

    [ update ]

--- restart.py
+++ restart.py
@@ -2,6 +2,11 @@ from selenium import webdriver
 import time
 import os
 
+options = webdriver.chrome.options.Options()
+options.add_argument('--headless')
+options.add_argument('--start-maximized')
+driver = webdriver.Chrome('/app/.chromedriver/bin/chromedriver', options=options)
+
 num = 1
 
 while num != 0:
@@ -10,18 +15,13 @@ while num != 0:
     ret = 0
 
     try:
-        options = webdriver.chrome.options.Options()
-        options.add_argument('--headless')
-        options.add_argument('--start-maximized')
-        driver = webdriver.Chrome('/app/.chromedriver/bin/chromedriver', options=options)
-
         driver.get('https://id.heroku.com/login')
         time.sleep(1)
         driver.find_element_by_id('email').send_keys(os.environ['herokuemail'])
         driver.find_element_by_id('password').send_keys(os.environ['herokupass'])
         driver.find_element_by_name('commit').click()
         print('login succeed')
-        time.sleep(30)
+        time.sleep(10)
         driver.find_element_by_xpath('/html/body/div[5]/main/div[2]/div[2]/div[3]/div[1]/a/div/span').click()
         print('select "waldo-translator"')
         time.sleep(5)
