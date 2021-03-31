b6a28795072e4bec57bed61d189a0e82cc1e9789
Young-woong Lazarus Sohn <44917049+syw0919@users.noreply.github.com>
2021-03-31 13:13:43 +0900

    [ update ]

--- restart.py
+++ restart.py
@@ -12,7 +12,6 @@ while num != 0:
     try:
         options = webdriver.chrome.options.Options()
         options.add_argument('--headless')
-        options.add_argument("--disable-gpu")
         options.add_argument('--start-maximized')
         driver = webdriver.Chrome('/app/.chromedriver/bin/chromedriver', options=options)
 
@@ -21,13 +20,17 @@ while num != 0:
         driver.find_element_by_id('email').send_keys(os.environ['herokuemail'])
         driver.find_element_by_id('password').send_keys(os.environ['herokupass'])
         time.sleep(20)
+        print('login succeed')
         driver.find_element_by_xpath('/html/body/div[5]/main/div[2]/div[2]/div[3]/div[1]/a/div/span').click()
+        print('select "waldo-translator"')
         time.sleep(5)
         driver.find_element_by_xpath('/html/body/div[5]/main/div[2]/div[1]/div[1]/div[2]/div/div[1]/button').click()
         time.sleep(1)
         driver.find_element_by_xpath('/html/body/div[5]/main/div[2]/div[1]/div[1]/div[2]/div/div[1]/div/li[9]/a').click()
+        print('select "Restart all dynos"')
         time.sleep(3)
         driver.find_element_by_xpath('/html/body/div[4]/div[1]/div/div/div[3]/button[2]').click()
+        print('execute')
         time.sleep(10)
 
     except Exception as ex:
