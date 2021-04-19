8bda4492c5c2639fc641f1b43a7cc203f5df0553
Young-woong Lazarus Sohn <44917049+syw0919@users.noreply.github.com>
2021-04-19 10:44:06 +0900

    [ update restart.py ]

--- restart.py
+++ restart.py
@@ -21,7 +21,7 @@ while num != 0:
         driver.find_element_by_id('password').send_keys(os.environ['herokupass'])
         driver.find_element_by_name('commit').click()
         print('login succeed')
-        time.sleep(20)
+        time.sleep(30)
         driver.find_element_by_xpath('/html/body/div[5]/main/div[2]/div[2]/div[3]/div[1]/a/div/span').click()
         print('select "waldo-translator"')
         time.sleep(5)
