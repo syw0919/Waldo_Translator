b6d87236d7a8fa58061701b5209c6af7173f22f6
Young-woong Lazarus Sohn <44917049+syw0919@users.noreply.github.com>
2021-04-19 16:43:30 +0900

    [ update restart.py ]

--- restart.py
+++ restart.py
@@ -5,6 +5,7 @@ import os
 options = webdriver.chrome.options.Options()
 options.add_argument('--headless')
 options.add_argument('--start-maximized')
+options.add_argument('--disable-gpu')
 driver = webdriver.Chrome('/app/.chromedriver/bin/chromedriver', options=options)
 
 num = 1
